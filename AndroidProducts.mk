abfe8bb4ecef6a1c881cb68f40eb54465aecf49f
FPSensor <gkartytgmail.com>
2023-01-23 23:03:44 +0000

    lavender: riceDroid Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    banana_lavender-user \
-    banana_lavender-userdebug \
-    banana_lavender-eng
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
